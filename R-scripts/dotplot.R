091e9aaf34c2485ade2d6afc927f068442971e43
Simon Butcher <s.butcher@qmul.ac.uk>
2017-07-12 13:46:08 +0100

    changed size, WIP

--- R-scripts/dotplot.R
+++ R-scripts/dotplot.R
@@ -1,5 +1,7 @@
-library(lattice)
 my1= read.csv("results.txt")
-dotplot(cpusecs ~ task, data = my1, main="Python performance in containers", pch=5, xlab="Task", ylab="Walltime/s")
+library(lattice)
+levels(my1$task) <- gsub(" ", "\n", levels(my1$task))
+png('rplot.png', width=1000, height=1000)
+dotplot(cpusecs ~ task, data = my1, main="Python performance in containers", pch=5, cex=2, cex.axis=2, xlab="Task", ylab="Walltime/s")
 
-
+dev.off()
