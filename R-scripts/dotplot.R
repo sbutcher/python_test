1b847dbd77d239ec90793d35da33173260ce9c31
Simon Butcher <s.butcher@qmul.ac.uk>
2017-07-13 20:41:53 +0100

    draw multi layer chart

--- R-scripts/dotplot.R
+++ R-scripts/dotplot.R
@@ -1,10 +1,23 @@
-my1= read.csv("/home/sbutcher/gits/python_test/R-scripts/results.txt")
+my1= read.csv("/home/sbutcher/gits/python_test/R-scripts/results_batch.txt")
+my2= read.csv("/home/sbutcher/gits/python_test/R-scripts/results.txt")
 library(lattice)
+library(latticeExtra)
+
 levels(my1$task) <- gsub(" ", "\n", levels(my1$task))
-png('rplot.png', height=800,width=800, units="px",pointsize=15, res=100)
-dotplot(cpusecs ~ task, data = my1, main="Python performance in containers (separate runs)", pch=5, cex=1.5, xlab="Task", ylab="Walltime/s")
+levels(my2$task) <- gsub(" ", "\n", levels(my2$task))
+
+png('rplot-both.png', height=800,width=800, units="px",pointsize=15, res=100)
+
+p <- dotplot(cpusecs ~ task, data = my1, main="Python performance in containers", pch=5, cex=1.5, xlab="Task type", ylab="Walltime/s", ylim=c(0,11000))
+p2 <- dotplot(cpusecs ~ task, data = my2, main="Python walltime results", pch=1, cex=1.5, xlab="Task type", ylab="Walltime/s", col="red", ylim=c(0000,11000), key=list(columns=2,
+        text=list(lab=c("separately run jobs","grouped array runs")),
+        points=list(pch=c(1,5), col=c("red", "blue")))
+        )
+# add second p2 plot as a layer
+p2 + as.layer(p)
 
 dev.off()
+# print mean values for tables
 print("mean cpusecs")
 mean(my1[1:10, ]$cpusecs)
 mean(my1[11:20, ]$cpusecs)
